011d6e39cde0d9124bd300dca6b02e099727e80a
AleksandrMum <142475782+AleksandrMum@users.noreply.github.com>
2025-09-25 16:33:50 +0300

    Мелкие правки docker-compose и dockerignore

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import db from "./database/index.js";
 import router from "./routes/index.js";
 
-const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || "127.0.0.1";
+const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
+const SERVER_PORT = process.env.SERVER_PORT || 8080;
 
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
@@ -16,6 +16,6 @@ db.client.sync({force: false, alter: false})
 
 app.use("", router(db));
 
-app.listen(PORT, HOST, () => {
-	console.log(`Server started on http://${HOST}:${PORT}`);
+app.listen(SERVER_PORT, SERVER_HOST, () => {
+	console.log(`Server started on http://${SERVER_HOST}:${SERVER_PORT}`);
 });
