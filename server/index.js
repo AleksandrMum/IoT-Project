23baa5a2de46cb096b807daf96e13f54a77960a6
AleksandrMum <142475782+AleksandrMum@users.noreply.github.com>
2025-10-13 16:13:02 +0300

    Добавление оболочки создания сервера

--- server/index.js
+++ server/index.js
@@ -6,16 +6,25 @@ import router from "./routes/index.js";
 const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
 const SERVER_PORT = process.env.SERVER_PORT || 8080;
 
-const app = express();
-app.use(cors({ origin: true, credentials: true }));
-app.use(express.json());
+const createApp = async () => {
+	const app = express();
+	app.use(cors({ origin: true, credentials: true }));
+	app.use(express.json());
 
-db.client.sync({force: false, alter: true})
-	.then(() => console.log("Database OK"))
-	.catch((err) => console.error("Database ERROR:", err));
+	await db.client.sync({force: false, alter: true})
+		.then(() => console.log("Database OK"))
+		.catch((err) => {
+			console.error("Database ERROR:", err)
+			process.exit(1);
+		});
 
-app.use("", router(db));
+	app.use("", router(db));
 
-app.listen(SERVER_PORT, SERVER_HOST, () => {
-	console.log(`Server started on http://${SERVER_HOST}:${SERVER_PORT}`);
-});
+	app.listen(SERVER_PORT, SERVER_HOST, () => {
+		console.log(`Server started on http://${SERVER_HOST}:${SERVER_PORT}`);
+	});
+
+	return app;
+}
+
+createApp();
